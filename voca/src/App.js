964eb9d33fc1da9e195cad8a502b8ea527d94231
강해빈 <meerhae@gmail.com>
2023-06-15 12:39:29 +0900

    230615 react 컴포넌트 생성 및 .module.css

--- voca/src/App.js
+++ voca/src/App.js
@@ -1,26 +1,18 @@
 
 import './App.css';
+import styles from './App.module.css';
 import Hello from './component/Hello';
+import Welcome from './component/Welcome';
 
 function App() {
-  const name = 'Haeven'
-  const naver = {
-    name: '네이버',
-    url: 'https://naver.com',
-  };
 
   return (
     <div className="App">
-      <h1
-        style={{
-          color: '#f0f',
-          backgroundColor: 'green',
-        }}
-      >
-        Welcome, {name}.
-        <p>{2 + 3}</p>
-      </h1>
-      <a href={naver.url}>{naver.name}</a>
+      <Welcome />
+
+      <Hello />
+
+      <div className={styles.box}>app</div>
     </div>
   );
 }
--- voca/src/component/Hello.js
+++ voca/src/component/Hello.js
@@ -1,3 +1,20 @@
+import World from "./World";
+import styles from "./Hello.module.css"
+
+
 export default function Hello() {
-  return <p>Hello</p>;
+  return (
+    <div>
+      <p style={{
+        color: '#f00',
+        borderRight: '2px solid #000',
+        marginBottom: '30px',
+        opacity: 0.5
+      }}>Hello</p>
+      <World />
+      <World />
+      <div className={styles.box}>hello</div>
+    </div>
+
+  );
 }
