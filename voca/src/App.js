48d111802ee842de09e430f6b1c17011b667e466
강해빈 <meerhae@gmail.com>
2023-06-15 22:56:40 +0900

    230615 react POST 생성, useNavigate()

--- voca/src/App.js
+++ voca/src/App.js
@@ -1,6 +1,8 @@
 import Day from "./component/Day";
 import DayList from "./component/DayList";
 import EmptyPage from "./component/EmptyPage";
+import CreateWord from "./component/CreateWord";
+import CreateDay from "./component/CreateDay";
 import Header from "./component/Header";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
@@ -13,7 +15,9 @@ function App() {
         <Routes>
           <Route path="/" element={<DayList />} />
           <Route path="/day/:day" element={<Day />} />
-          <Route path="/*" element={<EmptyPage />} /> {/* dfgdfgd */}
+          <Route path="/create_word" element={<CreateWord />} />
+          <Route path="/create_day" element={<CreateDay />} />
+          <Route path="/*" element={<EmptyPage />} /> {/* 나머지 전체 */}
         </Routes>
       </div>
     </BrowserRouter>
--- voca/src/component/DayList.js
+++ voca/src/component/DayList.js
@@ -1,21 +1,8 @@
 import { Link } from "react-router-dom";
 import useFetch from "../hooks/useFetch";
-// import { useEffect, useState } from "react";
 
 export default function DayList() {
   const days = useFetch("http://localhost:3001/days");
-
-  // const [days, setDays] = useState([]);
-
-  // useEffect(() => { // 어떤 상태값이 바뀌었을 때 동작하는 함수 작성
-  //   fetch('http://localhost:3001/days')
-  //     .then(res => {
-  //       return res.json();
-  //     })
-  //     .then(data => {
-  //       setDays(data);
-  //     })
-  // }, []); 
 
   return (
     <>
