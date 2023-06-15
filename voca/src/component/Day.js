ff2d27fd143556469f48d6075ce00d4dd6421d12
강해빈 <meerhae@gmail.com>
2023-06-15 17:19:22 +0900

    230615 react json-server 및 단어 뜻 보기 숨기기 구현

--- voca/src/component/Day.js
+++ voca/src/component/Day.js
@@ -1,5 +1,6 @@
 import dummy from "../db/data.json"
 import { useParams } from "react-router-dom";
+import Word from "./Word";
 
 export default function Day() {
 
@@ -16,10 +17,7 @@ export default function Day() {
       <table>
         <tbody>
           {wordList.map(word => (
-            <tr>
-              <td>{word.eng}</td>
-              <td>{word.kor}</td>
-            </tr>
+            <Word word={word} key={word.id} />
             ))}
         </tbody>
       </table>
