02d540c811629843606d7fe5b086def39ce0a997
강해빈 <meerhae@gmail.com>
2023-06-15 17:46:52 +0900

    230615 react useEffect, fetch()로 API 호출

--- voca/src/component/Day.js
+++ voca/src/component/Day.js
@@ -1,14 +1,24 @@
-import dummy from "../db/data.json"
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Word from "./Word";
 
 export default function Day() {
 
   const {day} = useParams();
-  const wordList = dummy.words.filter(word => (
-    word.day === Number(day)
-  ))
-  console.log(wordList);
+  // const wordList = dummy.words.filter(word => (
+  //   word.day === Number(day)
+  // ))
+  const [words, setWords] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/words?day=${day}`)
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        setWords(data);
+      });
+  }, [day]); // 의존성 배열에 값을 넣어 최신값이라는 걸 보장한다.
 
 
   return (
@@ -16,7 +26,7 @@ export default function Day() {
       <h2>Day {day}</h2>
       <table>
         <tbody>
-          {wordList.map(word => (
+          {words.map(word => (
             <Word word={word} key={word.id} />
             ))}
         </tbody>
--- voca/src/component/DayList.js
+++ voca/src/component/DayList.js
@@ -1,18 +1,32 @@
 import { Link } from "react-router-dom";
 import dummy from "../db/data.json";
+import { useEffect, useState } from "react";
 
 export default function DayList() {
-  console.log(dummy)
+  const [days, setDays] = useState([]);
+
+  useEffect(() => { // 어떤 상태값이 바뀌었을 때 동작하는 함수 작성
+    fetch('http://localhost:3001/days')
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        setDays(data);
+      })
+  }, []); 
+
   return (
-    <ul className="list_day">
-      {dummy.days.map(day => (
-        <li key={day.id}>
-          <Link to={`/day/${day.day}`}>
-            Day {day.day}
-          </Link>
-        </li>
-        ))}
-      <li></li>
-    </ul>
+    <>
+      <ul className="list_day">
+        {days.map(day => (
+          <li key={day.id}>
+            <Link to={`/day/${day.day}`}>
+              Day {day.day}
+            </Link>
+          </li>
+          ))}
+        <li></li>
+      </ul>
+    </>
   );
 }
