bc0d3bb0e77b1027beb3d79815b13cc541a248d3
kanghaeven <aubrienid@naver.com>
2023-07-07 00:11:15 +0900

    230706 react useEffect

--- movie_practice/src/App.js
+++ movie_practice/src/App.js
@@ -1,10 +1,29 @@
-import Button from "./Button";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  console.log("I run all the time.");
+  useEffect(() => {
+    console.log("CALL THE API...");
+  }, []);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("Search for", keyword);
+    }
+  }, [keyword]);
   return (
     <div className="App">
-      <h1>Welcome Back!</h1>
-      <Button text={"Continue"} />
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
