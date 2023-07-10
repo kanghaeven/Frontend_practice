0265bcf9974d7f40339f8c6d7251352ed56f5f37
kanghaeven <aubrienid@naver.com>
2023-07-10 21:28:25 +0900

    230710 React 학습  점진적으로 사용하는 방법 / 웹 브라우저 환경 with CDN / React, ReactDOM API

--- ssafy-react-me/02-react-babel-browser/scripts/main.js
+++ ssafy-react-me/02-react-babel-browser/scripts/main.js
@@ -1,9 +1,48 @@
 function main() {
-  console.log('HTML에서 React 시작하기! 😃');
+  const { createElement: h } = React;
+  const { createRoot } = ReactDOM;
+
+  // console.log('HTML에서 React 시작하기! 😃');
+
+  // React 요소 생성
+  const app = h(
+    "div",
+    {
+      className: "app",
+    },
+    h(
+      "figure",
+      {
+        className: "library",
+      },
+      h("img", {
+        src: "assets/ReactLogo.svg",
+        alt: "",
+      }),
+      h(
+        "strong",
+        {
+          className: "name",
+        },
+        "React"
+      ),
+      h(
+        "p",
+        {
+          className: "catchphrize",
+        },
+        "사용자 인터페이스 구축을 위한 웹 &amp; 모바일 라이브러리"
+      )
+    )
+  );
+
+  // ReactDOMRoot 객체 생성
+  const reactDOMRoot = createRoot(document.getElementById("root"));
+  reactDOMRoot.render(app);
 }
 
 function handleGoToReference() {
-  globalThis.open('https://react.dev', '_blank');
+  globalThis.open("https://react.dev", "_blank");
 }
 
 main();
