9c6ce38e6accfe45b13bd0a5a760917fba433d29
DESKTOP-DOGVPUB\SSAFY <meerhae@gmail.com>
2023-07-11 15:11:11 +0900

    230711 ESLint 플러그인 설치 및 구성 / 에셋 활용 방법  @eslint/config 설치 구성 / public, src/assets 디렉토리 에셋 관

--- ssafy-react-me/03-react-with-vite/src/App.jsx
+++ ssafy-react-me/03-react-with-vite/src/App.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import reactLogoPath from './assets/ReactLogo.svg'
 
-const App = () => (
-  <div>
+
+function App() {
+  return (
+    <div>
     <h1 className="headline">React is Awesome</h1>
+    <h2>정적 에셋</h2>
+    <img src="/ReactLogo.svg" alt="React Logo" />
+    <h2>동적 에셋</h2>
+    <img src={reactLogoPath} alt="React Logo Path" />
   </div>
-);
+  );
+}
 
 export default App;
