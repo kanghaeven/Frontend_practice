63023b3b366856fb874116eb0f4735d1078e6932
DESKTOP-DOGVPUB\SSAFY <meerhae@gmail.com>
2023-07-12 12:56:29 +0900

    230712 ESLint 플러그인 추가 설치 및 구성  eslint-plugin-jsx-a11y / eslint-plugin-react-hooks / eslint-plugin-react-fresh / eslint-plugin-import Type 정의 파일 설치 @types/node

--- ssafy-react-me/03-react-with-vite/.eslintrc.cjs
+++ ssafy-react-me/03-react-with-vite/.eslintrc.cjs
@@ -9,7 +9,8 @@ module.exports = {
         "plugin:react/recommended",
         "plugin:react/jsx-runtime",
         "plugin:react-hooks/recommended",
-        "plugin:jsx-a11y/recommended"
+        "plugin:jsx-a11y/recommended",
+        "import/recommended"
     ],
     "overrides": [
         {
@@ -32,11 +33,18 @@ module.exports = {
         "react",
         "jsx-a11y",
         "react-hooks",
-        'react-refresh'
+        'react-refresh',
+        'import'
     ],
     "rules": {
         'react-hooks/rules-of-hooks': "error",
-        'react-hooks/exhaustive-deps':' "warn",
-        'react-refresh/only-export-components':' "warn"
+        'react-hooks/exhaustive-deps': "warn",
+        'react-refresh/only-export-components': "warn",
+        'import/no-unresolved': [2, {commonjs: true, amd: true}],
+        'import/named': 2,
+        'import/namespace': 2,
+        'import/default': 2,
+        'import/export': 2
+
     }
 }
