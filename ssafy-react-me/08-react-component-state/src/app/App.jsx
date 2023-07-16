64e854bce45aaa95a9af9f963cea0c385fa08842
kanghaeven <meerhae@gmail.com>
2023-07-16 17:42:13 +0900

    230716 React 상태관리

--- ssafy-react-me/08-react-component-state/src/app/App.jsx
+++ ssafy-react-me/08-react-component-state/src/app/App.jsx
@@ -1,43 +1,22 @@
-import React from "react";
-import styles from "./App.module.css";
-import { ScrollButton, DescriptionList } from "@/components";
+import React from 'react';
 
-const isShowReactImage = true;
-
-const statusMessage = [
-  "⌛️ 대기",
-  "⏳ 로딩 중...",
-  "✅ 로딩 성공!",
-  "❌ 로딩 실패.",
-];
-
-const reactLibrary = {
-  name: "React",
-  author: "조던 워케(Jordan Walke)",
-  writtenIn: "JavaScript",
-  type: "JavaScript 라이브러리",
-  license: "MIT",
-};
-
-const renderList = ({ isReverse = false } = {}) => {
-  const data = !isReverse ? statusMessage : statusMessage.reverse();
-  return data.map((message, index) => <li key={index}>{message}</li>);
-};
+import styles from './App.module.css';
+import { ScrollButton, DescriptionList } from '@/components';
 
 const handleScrollMove = ({ currentTarget, target }) => {
   const { top } = currentTarget.getBoundingClientRect();
-  const appElement = document.getElementById("root")?.firstElementChild;
+  const appElement = document.getElementById('root')?.firstElementChild;
 
   if (target.matches('[title="스크롤 다운"]')) {
     appElement.scroll({
       top,
-      behavior: "smooth",
+      behavior: 'smooth',
     });
   }
   if (target.matches('[title="스크롤 업"]')) {
     appElement.scroll({
       top: 0,
-      behavior: "smooth",
+      behavior: 'smooth',
     });
   }
 };
@@ -51,30 +30,103 @@ const handleScrollMove = ({ currentTarget, target }) => {
 // 컴포넌트 다시 렌더링 (하위에 포함된 컴포넌트 모두 다시 렌더링 : 성능 저하)
 
 function App() {
-  const [imageType, setImageType] = React.useState("react");
+  const [imageType, setImageType] = React.useState('react');
+  const [isShowReactImage, setIsShowReactImage] = React.useState(true);
+
+  const [descKey, setDescKey] = React.useState(9);
+  const [statusMessage, setStatusMessage] = React.useState([
+    '⌛️ 대기',
+    '⏳ 로딩 중...',
+    '✅ 로딩 성공!',
+    '❌ 로딩 실패.',
+  ]);
+
+  const handleAddStatusMessage = (newStatusMessage) => {
+    setStatusMessage([newStatusMessage, ...statusMessage]);
+  };
+
+  const handleDeleteStatusMessage = (deleteIndex) => {
+    setStatusMessage(statusMessage.filter((_, i) => i !== deleteIndex));
+  };
+
+  const renderList = ({ isReverse = false } = {}) => {
+    // mutable ❌
+    // immutable ✅
+    const data = !isReverse ? statusMessage : [...statusMessage].reverse();
+
+    return data.map((message, index) => (
+      <li key={index}>
+        {message}
+        <button
+          onClick={() => {
+            handleDeleteStatusMessage(
+              !isReverse ? index : data.length - (index + 1)
+            );
+          }}
+        >
+          delete
+        </button>
+      </li>
+    ));
+  };
+
+  const [reactLibrary, setReactLibrary] = React.useState({
+    name: 'React',
+    author: '조던 워케(Jordan Walke)',
+    writtenIn: 'JavaScript',
+    type: 'JavaScript 라이브러리',
+    license: 'MIT',
+  });
+
+  const handleUpdateReactLibrary = (newReactLibrary) => {
+    setReactLibrary(newReactLibrary);
+  };
 
   return (
     <div className={styles.container}>
       <h1>React 컴포넌트 상태 관리</h1>
-      <button
-        type="button"
-        onClickCapture={() => {
-          // DOM 요소 접근/조작
-          // 사이드 이펙트 처리
-          // 선언된 상태 업데이트 → UI 리-렌더링
-          setImageType(imageType === "react" ? "vite" : "react");
-        }}
-      >
-        change imageType
-      </button>
+      <div style={{ display: 'flex', columnGap: 8 }}>
+        <button
+          type="button"
+          onClick={() => {
+            const newDescKey = descKey + 3;
+            setDescKey(newDescKey);
+            console.log({ newDescKey });
+          }}
+        >
+          reload DescriptionList component
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            setIsShowReactImage(!isShowReactImage);
+          }}
+        >
+          {isShowReactImage ? '리액트 이미지 감춤' : '리액트 이미지 표시'}
+        </button>
+        <button
+          type="button"
+          onClickCapture={() => {
+            // DOM 요소 접근/조작
+            // 사이드 이펙트 처리
+            // 선언된 상태 업데이트 → UI 리-렌더링
+            setImageType(imageType === 'react' ? 'vite' : 'react');
+          }}
+        >
+          change imageType
+        </button>
+      </div>
       <hr />
       <DescriptionList
+        key={descKey}
         {...{
           statusMessage,
           imageType,
           isShowReactImage,
           renderList,
           reactLibrary,
+          onAddStatusMessage: handleAddStatusMessage,
+          onUpdateReactLibrary: handleUpdateReactLibrary,
         }}
       />
       <ScrollButton.Group onScroll={handleScrollMove}>
--- ssafy-react-me/08-react-component-state/src/components/DescriptionList/DescriptionList.jsx
+++ ssafy-react-me/08-react-component-state/src/components/DescriptionList/DescriptionList.jsx
@@ -1,15 +1,36 @@
-import { arrayOf, bool, exact, func, oneOf, string } from 'prop-types';
-import styles from './DescriptionList.module.css';
 import vitePath from '@/assets/vite.svg';
+import { arrayOf, bool, exact, func, oneOf, string } from 'prop-types';
+import { useState } from 'react';
+import styles from './DescriptionList.module.css';
 import reactPath from '/react.svg';
 
 function DescriptionList({
   statusMessage,
+  onAddStatusMessage,
   imageType,
   isShowReactImage,
   renderList,
   reactLibrary,
+  onUpdateReactLibrary,
 }) {
+  const [editMode, setEditMode] = useState(false);
+
+  const handleAddStatusMessage = (e) => {
+    e.preventDefault();
+    const { newStatusMessage } = e.target;
+
+    const { value } = newStatusMessage;
+
+    // 입력 조건 (새로운 상태 메시지의 값이 0보다 크다)
+    if (value.trim().length > 0) {
+      // 새로운 상태 메시지로 추가
+      onAddStatusMessage?.(value);
+      newStatusMessage.value = '';
+    } else {
+      newStatusMessage.select();
+    }
+  };
+
   return (
     <dl className={styles.container}>
       <dt>데이터 바인딩(data binding)</dt>
@@ -54,6 +75,13 @@ function DescriptionList({
       </dd>
       <dt>리스트 렌더링(list rendering)</dt>
       <dd>
+        <form onSubmit={handleAddStatusMessage}>
+          <div className="formControl">
+            <label htmlFor="newStatusMessage">새로운 상태 메시지</label>
+            <input id="newStatusMessage" name="newStatusMessage" type="text" />
+          </div>
+          <button type="submit">추가</button>
+        </form>
         <p>상태 메시지(status message) 배열을 리스트 렌더링합니다.</p>
         <ul className={styles.renderList}>{renderList()}</ul>
       </dd>
@@ -66,12 +94,58 @@ function DescriptionList({
           React 라이브러리(reactLibrary) 객체의 키, 값을 리스트 렌더링합니다.
         </p>
         <dl className={styles.reactLibrary}>
-          {Object.entries(reactLibrary).map(([key, value]) => (
-            <div key={key}>
-              <dt>{key}</dt>
-              <dd>{value}</dd>
+          {!editMode ? (
+            <>
+              {Object.entries(reactLibrary).map(([key, value]) => (
+                <div key={key}>
+                  <dt>{key}</dt>
+                  <dd>{value}</dd>
+                </div>
+              ))}
+              <button
+                type="button"
+                style={{ marginTop: 12 }}
+                onClick={() => setEditMode(true)}
+              >
+                수정
+              </button>
+            </>
+          ) : (
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'flex-start',
+              }}
+            >
+              <h2>에디트 모드</h2>
+              <form className="editForm">
+                {Object.entries(reactLibrary).map(([key, value]) => (
+                  <div key={key} className="formControl">
+                    <label htmlFor={key}>{key}</label>
+                    <input id={key} type="text" defaultValue={value} />
+                  </div>
+                ))}
+              </form>
+              <button
+                type="button"
+                onClick={() => {
+                  const editForm = document.querySelector('.editForm');
+                  const { name, author, writtenIn, type, license } = editForm;
+                  onUpdateReactLibrary({
+                    name: name.value,
+                    author: author.value,
+                    writtenIn: writtenIn.value,
+                    type: type.value,
+                    license: license.value,
+                  });
+                  setEditMode(false);
+                }}
+              >
+                저장
+              </button>
             </div>
-          ))}
+          )}
         </dl>
       </dd>
     </dl>
@@ -84,11 +158,13 @@ DescriptionList.defaultProps = {
 
 DescriptionList.propTypes = {
   statusMessage: arrayOf(
-    oneOf(['⌛️ 대기', '⏳ 로딩 중...', '✅ 로딩 성공!', '❌ 로딩 실패.'])
+    string
+    // oneOf(['⌛️ 대기', '⏳ 로딩 중...', '✅ 로딩 성공!', '❌ 로딩 실패.'])
   ).isRequired,
+  onAddStatusMessage: func,
   imageType: oneOf(['react', 'vite']).isRequired,
   isShowReactImage: bool.isRequired,
-  renderList: func,
+  renderList: func, // () => void | undefined
   reactLibrary: exact({
     name: string,
     author: string,
@@ -96,6 +172,7 @@ DescriptionList.propTypes = {
     type: string,
     license: string,
   }).isRequired,
+  onUpdateReactLibrary: func,
 };
 
 export default DescriptionList;
